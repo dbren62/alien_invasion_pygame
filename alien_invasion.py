a4dd440fca1c6ef428afc8f17916383d64314547
David Brennan <david_brennan1@baylor.edu>
2021-03-07 11:11:45 -0600

    initial commit

---
+++ alien_invasion.py
@@ -0,0 +1,12 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    #Overall class to manage game assets and bahvior.
+
+    def __init__(self):
+        #Initializes the game, and create game resources
+        pygame.init()
+
+    
