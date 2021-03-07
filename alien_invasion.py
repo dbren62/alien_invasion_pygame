6982fae30d168acd6825db303d8c72eb498c87d2
David Brennan <david_brennan1@baylor.edu>
2021-03-07 16:36:15 -0600

    "Q" to quit

--- alien_invasion.py
+++ alien_invasion.py
@@ -42,6 +42,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_Q:
+            sys.exit()
             
     def _check_keyup_events(self, event):
         """Respond to key releases."""
