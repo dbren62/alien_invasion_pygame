ef707f2eadf19cdbea339638f64efb5c173b34c1
David Brennan <david_brennan1@baylor.edu>
2021-03-13 12:42:55 -0600

    set dynamic settings and speed_scale

--- alien_invasion.py
+++ alien_invasion.py
@@ -135,6 +135,7 @@ class AlienInvasion:
             #destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """Check if the fleet is at an edge,
