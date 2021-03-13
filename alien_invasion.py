91e3c88737a72cba279a3343403ef76910699698
David Brennan <david_brennan1@baylor.edu>
2021-03-13 13:11:17 -0600

    initialize scoreboard on screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -29,8 +30,10 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         """
 
-        #create an instance to store game statistics.
+        #create an instance to store game statistics,
+        # and create a scoreboard.
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         #create instance of ship after screen has been created, giving it access to the game's resources with (self) parameter
         self.ship = Ship(self)
@@ -226,6 +229,9 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        #draw the score information.
+        self.sb.show_score()
 
         #draw the play button if the game is inactive.
         if not self.stats.game_active:
